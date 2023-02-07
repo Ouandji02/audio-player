c42a86d5ca3f41b7b1e621595adf2845aa5fac5e
ouandji02 <ouandjithierry00@gmail.com>
2023-02-07 15:18:38 +0100

    mediaservice connection

---
+++ app/src/main/java/com/phone/audioplayer/media/exoPlayer/MediaPlayerServiceConnection.kt
@@ -0,0 +1,123 @@
+package com.phone.audioplayer.media.exoPlayer
+
+import android.content.ComponentName
+import android.content.Context
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import com.phone.audioplayer.domain.model.Audio
+import com.phone.audioplayer.media.constant.K
+import com.phone.audioplayer.media.services.MediaPlayerService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MediaPlayerServiceConnection(context: Context) {
+    private val _playBackState: MutableStateFlow<PlaybackStateCompat?> = MutableStateFlow(null)
+    val playBackState: StateFlow<PlaybackStateCompat?>
+        get() = _playBackState
+    private var _isConnected: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isConnected: StateFlow<Boolean>
+        get() = _isConnected
+    val currentAudioPlaying: MutableStateFlow<Audio?> = MutableStateFlow(null)
+
+    lateinit var mediaControllerCompat: MediaControllerCompat
+
+    private val mediaBrowserServiceCallBack = MediaBrowserConnectionCallBack(context)
+
+    private val mediaBrowser = MediaBrowserCompat(
+        context,
+        ComponentName(context, MediaPlayerService::class.java),
+        mediaBrowserServiceCallBack,
+        null
+    ).apply {
+        connect()
+    }
+
+    private var audioList = listOf<Audio?>()
+
+    val rootMediaId: String
+        get() = mediaBrowser.root
+
+    val transportControl: MediaControllerCompat.TransportControls
+        get() = mediaControllerCompat.transportControls
+
+    fun playAudio(audios: List<Audio?>) {
+        audioList = audios
+        mediaBrowser.sendCustomAction(K.START_MEDIA_PLAY_ACTION, null, null)
+    }
+
+    fun fastForward(seconds: Int = 10) {
+        _playBackState.value?.currentPosition?.let {
+            transportControl.seekTo(it + seconds * 1000)
+        }
+    }
+
+    fun rewind(seconds: Int = 10) {
+        _playBackState.value?.currentPosition?.let {
+            transportControl.seekTo(it - seconds * 1000)
+        }
+    }
+
+    fun skipNext() {
+        transportControl.skipToNext()
+    }
+
+    fun skipPrevious() {
+        transportControl.skipToPrevious()
+    }
+
+    fun subscribe(parentId: String, callBack: MediaBrowserCompat.SubscriptionCallback) {
+        mediaBrowser.subscribe(parentId, callBack)
+    }
+
+    fun unSubscribe(parentId: String, callBack: MediaBrowserCompat.SubscriptionCallback) {
+        mediaBrowser.unsubscribe(parentId, callBack)
+    }
+
+    fun refreshMediaBrowserChildren() {
+        mediaBrowser.sendCustomAction(K.REFRESH_MEDIA_PLAY_ACTION, null, null)
+    }
+
+    private inner class MediaBrowserConnectionCallBack(private val context: Context) :
+        MediaBrowserCompat.ConnectionCallback() {
+        override fun onConnected() {
+            _isConnected.value = true
+            mediaControllerCompat = MediaControllerCompat(
+                context,
+                mediaBrowser.sessionToken
+            ).apply {
+                registerCallback()
+            }
+        }
+
+        override fun onConnectionSuspended() {
+            _isConnected.value = false
+        }
+
+        override fun onConnectionFailed() {
+            _isConnected.value = false
+        }
+    }
+
+    inner class MediaControllerCallBack : MediaControllerCompat.Callback() {
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+            super.onPlaybackStateChanged(state)
+            _playBackState.value = state
+        }
+
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+            super.onMetadataChanged(metadata)
+            currentAudioPlaying.value = metadata?.let { data ->
+                audioList.find {
+                    it?.id.toString() == data.description.mediaId
+                }
+
+            }
+        }
+
+        override fun onSessionDestroyed() {
+            super.onSessionDestroyed()
+        }
+    }
+}
